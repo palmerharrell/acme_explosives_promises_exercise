84364685745778d761bb439fa7068ed86408e793
Palmer Harrell <palmerharrell@me.com>
2016-03-02 18:01:47 -0600

    Finished JSON files.

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,13 @@
 "use strict";
 
+// Create a simple user interface for your product catalog where a user can select
+// a category from a dropdown.
+
+// When a category is selected, you must use Promises to read, first, from the categories.json
+// to load that array of objects, then load types.json, then products.json.
+
+// Once all data is loaded, you need to display the products in a Bootstrap grid.
+
+// Each product must display the string name of its product type, and product category,
+// not the integer id value.
+
