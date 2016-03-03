46baafc1b2586cbc2e04618ee5eab06cbf452023
Palmer Harrell <palmerharrell@me.com>
2016-03-02 23:28:10 -0600

    FInished Promises exercise

--- scripts/app.js
+++ scripts/app.js
@@ -1,138 +1,117 @@
 "use strict";
 
-
-// Once all data is loaded, you need to display the products in a Bootstrap grid.
-
-// Each product must display the string name of its product type, and product category,
-// not the integer id value.
-
 let categories = [];
 let types = [];
 let products = [];
 let categoriesHTML = ``;
 let productsHTML = ``;
 
-// Process AJAX requests
-let loadCategories = function() {
-  return new Promise((resolve, reject) => {
-  	$.ajax({url:"./jsonFiles/categories.json"}).done(function(catsObj) {
-  		resolve(catsObj);
-  	});
-  });
-};
+$(document).ready(function() {
 
-let loadTypes = function() {
-  return new Promise((resolve, reject) => {
-  	$.ajax({url:"./jsonFiles/types.json"}).done(function(typesObj) {
-  		resolve(typesObj);
-  	});
-  });
-};
+	// Process AJAX requests
+	let loadCategories = function() {
+	  return new Promise((resolve, reject) => {
+	  	$.ajax({url:"./jsonFiles/categories.json"}).done(function(catsObj) {
+	  		resolve(catsObj);
+	  	});
+	  });
+	};
 
-let loadProducts = function() {
-  return new Promise((resolve, reject) => {
-  	$.ajax({url:"./jsonFiles/products.json"}).done(function(prodsObj) {
-  		products = prodsObj.products;
-  		resolve();
-  	});
-  });
-};
+	let loadTypes = function() {
+	  return new Promise((resolve, reject) => {
+	  	$.ajax({url:"./jsonFiles/types.json"}).done(function(typesObj) {
+	  		resolve(typesObj);
+	  	});
+	  });
+	};
 
-
-// Reject Function
-let handleRejection = function() {
-	console.log("Promise Rejected");
-};
+	let loadProducts = function() {
+	  return new Promise((resolve, reject) => {
+	  	$.ajax({url:"./jsonFiles/products.json"}).done(function(prodsObj) {
+	  		products = prodsObj.products;
+	  		resolve();
+	  	});
+	  });
+	};
 
 
-// Load JSON files in order
-function loadJSON() {
-	loadCategories().then(
-	  function (jsonObj) {
-	    categories = jsonObj.categories;
-	    return loadTypes();
-	  }, handleRejection
-	).then(
-	  function (jsonObj) {
-	    types = jsonObj.types;
-	    return loadProducts();
-	  }, handleRejection
-	).then(
-	  function () {
-	  	// Populate Category Select with available categories
-			for (let i = 0; i < categories.length; i++) {
-				categoriesHTML += `<option value="${categories[i].name}">${categories[i].name}</option>`;
-			}
-			$("#categories").append(categoriesHTML);
-	  }, handleRejection
-	);
-}
-
-
-// Populate .all_products div
-function populateProducts(selProducts, selTypes, selCategory) {
-
-	for (let i = 0; i < selProducts.length; i++) {
-		var currentType = selTypes.filter(function(obj) {
-  		return obj.id == selProducts[i].type;
-		});
-
-		console.log(selProducts[i].name);
-		console.log(currentType[0].name);
-		console.log("Category:", selCategory);
-		console.log("~~~~~~~~~~~~~~~~~");
-
+	// Reject Function
+	function handleRejection() {
+		console.log("Promise Rejected");
 	}
-}
-
-loadJSON();
 
 
+	// Load JSON files in order
+	function loadJSON() {
+		loadCategories().then(
+		  function (jsonObj) {
+		    categories = jsonObj.categories;
+		    return loadTypes();
+		  }, handleRejection
+		).then(
+		  function (jsonObj) {
+		    types = jsonObj.types;
+		    return loadProducts();
+		  }, handleRejection
+		).then(
+		  function () {
+		  	// Populate Category Select with available categories
+				for (let i = 0; i < categories.length; i++) {
+					categoriesHTML += `<option value="${categories[i].name}">${categories[i].name}</option>`;
+				}
+				$("#categories").append(categoriesHTML);
+		  }, handleRejection
+		);
+	}
 
-// Event Listener
 
-$("#categories").change(function(){
-
-	let selectedCategory = $("#categories").val();
-	let categoryID = null;
-	let matchingTypes = [];
-	let matchingProducts = [];
-
-	for (let i = 0; i < categories.length; i++) {
-		if (selectedCategory === categories[i].name) {
-			categoryID = categories[i].id;
+	// Populate .all_products div
+	function populateProducts(selProducts, selTypes, selCategory) {
+		productsHTML = `<div class="row">`;
+		for (let i = 0; i < selProducts.length; i++) {
+			let currentType = selTypes.filter(function(obj) {
+	  		return obj.id == selProducts[i].type;
+			});
+			productsHTML += `<div class="col-md-5 product">`;
+			productsHTML += `<p class="name">${selProducts[i].name}</p>`;
+			productsHTML += `<p class="type">${currentType[0].name}</p>`;
+			productsHTML += `<p class="category">${selCategory}</p></div>`;
 		}
+		productsHTML += `</div>`;
+		$(".all_products").html(productsHTML);
 	}
 
-	for (let i = 0; i < types.length; i++) {
-		if (categoryID === types[i].category) {
-			matchingTypes.push(types[i]);
-		}
-	}
-
-	for (let i = 0; i < products.length; i++) {
-		for (let j = 0; j < matchingTypes.length; j++) {
-			if (products[i].type == matchingTypes[j].id) {
-				matchingProducts.push(products[i]);
+	let getMatches = function() {
+		let selectedCategory = $("#categories").val();
+		let categoryID = null;
+		let matchingTypes = [];
+		let matchingProducts = [];
+		
+		for (let i = 0; i < categories.length; i++) {
+			if (selectedCategory === categories[i].name) {
+				categoryID = categories[i].id;
 			}
 		}
-	}
-	populateProducts(matchingProducts, matchingTypes, selectedCategory);
+
+		for (let i = 0; i < types.length; i++) {
+			if (categoryID === types[i].category) {
+				matchingTypes.push(types[i]);
+			}
+		}
+
+		for (let i = 0; i < products.length; i++) {
+			for (let j = 0; j < matchingTypes.length; j++) {
+				if (products[i].type == matchingTypes[j].id) {
+					matchingProducts.push(products[i]);
+				}
+			}
+		}
+		populateProducts(matchingProducts, matchingTypes, selectedCategory);
+	};
+
+
+	loadJSON();
+	$("#categories").change(getMatches);
+
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
