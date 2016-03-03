3be5ac773e7c90c5fd6c69e77c766542e366da7e
Palmer Harrell <palmerharrell@me.com>
2016-03-02 19:07:22 -0600

    Set up Promise and ajax requests

--- scripts/app.js
+++ scripts/app.js
@@ -11,3 +11,64 @@
 // Each product must display the string name of its product type, and product category,
 // not the integer id value.
 
+var categories = [];
+var types = [];
+var products = [];
+
+// Process AJAX requests
+var loadCategories = function() {
+  return new Promise((resolve, reject) => {
+  	$.ajax({url:"./jsonFiles/categories.json"}).done(function(catsObj) {
+  		resolve(catsObj);
+  	});
+  });
+};
+
+var loadTypes = function() {
+  return new Promise((resolve, reject) => {
+  	$.ajax({url:"./jsonFiles/types.json"}).done(function(typesObj) {
+  		resolve(typesObj);
+  	});
+  });
+};
+
+var loadProducts = function() {
+  return new Promise(() => {
+  	$.ajax({url:"./jsonFiles/products.json"}).done(function(prodsObj) {
+  		products = prodsObj.products;
+  	});
+  });
+};
+
+
+// Reject Function
+var handleRejection = function() {
+	console.log("Promise Rejected");
+};
+
+
+// Load JSON files in order
+function loadJSON() {
+	loadCategories().then(
+	  function (jsonObj) {
+	    categories = jsonObj.categories;
+	    return loadTypes();
+	  }, handleRejection
+	).then(
+	  function (jsonObj) {
+	    types = jsonObj.types;
+	    return loadProducts();
+	  }, handleRejection
+	);
+}
+
+loadJSON();
+
+// Event Listener
+
+
+
+
+
+
+
